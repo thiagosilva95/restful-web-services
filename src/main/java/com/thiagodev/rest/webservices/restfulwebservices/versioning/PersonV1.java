7f03bc2c81acd59e0b7f139b28d573c0c60c2ea9
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-10-09 00:02:30 -0300

    versioning api

---
+++ src/main/java/com/thiagodev/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,25 @@
+package com.thiagodev.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV1 {
+
+	private String name;
+
+	public PersonV1() {
+	}
+	
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
